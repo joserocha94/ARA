344975f64189af52586755cdb9056ef735b01914
José Rocha <48589830+joserocha94@users.noreply.github.com>
2022-09-27 00:49:41 +0100

    find euler circuit

--- euler.cpp
+++ euler.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+// add_egde should add the edge in the both vertices
+// create and delete edge method doing the opposite
 struct Graph
 {
     int v;
@@ -32,81 +34,43 @@ struct Graph
     }
 };
 
-
+// euler algorithm finds an euler circuit in graph;
+// each edge is traveled only once
+// add degree validation!
 void euler(Graph g, int start_node)
 {
-    int new_header, new_tail, aux; // aux does nothing
+    int new_header, new_tail, aux;
     std::vector<int> head;
     std::vector<int> tail;
 
     head.insert(head.begin(), start_node);
 
-    //for (int i=0; i<head.size(); i++)
-    //    printf("\n%d", head[i]);
-
     while (head.size())
     {
+        // while top-head is non-isolated node
+        // get edge uv, remove it, add v to the head
         while (g.adj[head.front()].size())
         {
-            printf("\n[working on %d, remove edge %d --- %d, add %d to head]", 
-                head.front(), 
-                head.front(), 
-                g.adj[head.front()].back(), 
-                g.adj[head.front()].back()); 
-            fflush(stdout);
             new_header = g.adj[head.front()].back();
             
             aux = head.front();
             g.adj[head.front()].pop_back();
             head.insert(head.begin(), new_header);
 
-            // find related ege
-            int counter = 0;
-            for (auto it:g.adj[new_header])
-            {
-                if (it == aux)
-                {
-                    aux = it;
-                    break;
-                }
-                counter++;
-            }
-
-            // remove it
-            printf("\nremove from index %d", counter);
-
-
-            cout << endl;
-            cout << endl;
-            g.print();
-
-            break;
+            g.adj[new_header].remove(aux);
         }    
-break;
-        // While top node v of head is isolated and head is not empty 
+
+        // while top-head isn't isolated or empty
+        // remove node from head, add node from tail
         while (!g.adj[head.front()].size() && head.size())
         {
-            printf("\n=================================");
-            printf("\nhead: %d", head.front());
-            printf("\ntop-node-neighbours: %d,  head-size: %d", g.adj[head.front()].size(), head.size());
-
             new_tail = head.front();
             tail.insert(tail.begin(), new_tail);
-            head.erase(head.begin());
-
-            printf("\nhead: %d", head.front());
-            printf("\ntop-node-neighbours: %d,  head-size: %d", g.adj[head.front()].size(), head.size());
-            printf("\n=================================");
-            
-            cout << endl;
-            cout << endl;
+            head.erase(head.begin());          
         }
     }
-    
-    cout << endl;
-    for (int i=0; i<head.size(); i++)
-        printf("\t%d", head[i]);
 
+    // print the founded euler circuit
     cout << endl;
     for (int i=0; i<tail.size(); i++)
         printf("\t%d", tail[i]);
@@ -116,16 +80,31 @@ break;
 
 int main()
 {
-    Graph g(4);
+    Graph g(6);
 
     g.add_edge(0, 1);
-    g.add_edge(0, 2);
+    g.add_edge(0, 5);
+
     g.add_edge(1, 0);
     g.add_edge(1, 2);
-        g.add_edge(2, 1);
-    g.add_edge(2, 0);
+    g.add_edge(1, 3);
+    g.add_edge(1, 5);
+
+    g.add_edge(2, 1);
     g.add_edge(2, 3);
-    g.add_edge(3, 2);
+
+    g.add_edge(3, 1);
+    g.add_edge(3, 2);    
+    g.add_edge(3, 4);
+    g.add_edge(3, 5);
+
+    g.add_edge(4, 3);
+    g.add_edge(4, 5);
+
+    g.add_edge(5, 0);
+    g.add_edge(5, 1);
+    g.add_edge(5, 3);
+    g.add_edge(5, 4);
 
     g.print();
     euler(g, 1);
